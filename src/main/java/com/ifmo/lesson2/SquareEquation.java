ddc3fd188e0cd4f157e1f8493f9d5c96a3ac81ef
korolevpl <korolev.home@gmail.com>
2021-10-16 18:16:18 +0300

    Task 'SquareEquation' for lesson 2 is done.

--- src/main/java/com/ifmo/lesson2/SquareEquation.java
+++ src/main/java/com/ifmo/lesson2/SquareEquation.java
@@ -27,10 +27,11 @@ public class SquareEquation {
      */
     // a = 3.0, b = -18.0, c = 27.0 ==> expected: <[3.0]> but was: <[0.0, 27.0]>]
     public static double[] squareEquationRoots(double a, double b, double c) {
-        double[] result = new double[2];
-        double discriminant = Math.pow(b, 2) - (4 * a * c);
 
+        double discriminant = Math.pow(b, 2) - (4 * a * c);
+        double[] result = new double[1];
         if (discriminant > 0) {
+            result = new double[2];
             result[0] = (-b - Math.sqrt(discriminant)) / (2 * a);
             result[1] = (-b + Math.sqrt(discriminant)) / (2 * a);
         } else if (discriminant == 0) {
