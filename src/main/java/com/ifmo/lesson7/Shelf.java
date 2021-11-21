9c4d8f8c19303df87caf26696e6a1158251849eb
korolevpl <korolev.home@gmail.com>
2021-11-22 00:14:36 +0300

    Commit after recovery Lesson 7.

---
+++ src/main/java/com/ifmo/lesson7/Shelf.java
@@ -0,0 +1,28 @@
+package com.ifmo.lesson7;
+
+public class Shelf {
+    Book book;
+    int quantity;
+    Shelf next;
+
+    public Shelf(Book book, int quantity) {
+        this.book = book;
+        this.quantity = quantity;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
